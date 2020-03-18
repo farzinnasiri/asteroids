db760ff206b57b924c6286f73679055b2bae059f
Farzin Nasiri <farzinnasiri890@gmail.com>
2020-03-19 00:55:08 +0330

    some things added

---
+++ src/Vector2d.java
@@ -0,0 +1,29 @@
+public class Vector2d {
+    private int x, y;
+
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2d(){
+        this(0,0);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
